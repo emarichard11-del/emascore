8e3224dbea2badefbc49e439e92818756e8e8bb9
emarichard11-del <ton-email@gmail.com>
2026-05-24 06:59:23 -0400

    haute confiance basée sur 1X2 seulement

--- server.py
+++ server.py
@@ -86,7 +86,7 @@ class Handler(BaseHTTPRequestHandler):
             aG = away_goals_for / away_total
             btts = min(80, max(30, round((hG * 0.7 + aG * 0.7) * 45 + 10)))
             over25 = min(78, max(25, round(avg_goals * 18 + 15)))
-            best_market = max([('1', prob1), ('X', probX), ('2', prob2), ('BTTS', btts), ('Over2.5', over25)], key=lambda x: x[1])
+            best_market = max([('1', prob1), ('X', probX), ('2', prob2)], key=lambda x: x[1])
             high_confidence = best_market[1] >= 75
             return {
                 'prob1': prob1, 'probX': probX, 'prob2': prob2,
