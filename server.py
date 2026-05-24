e174409a664072756f8c1816aea203feb61169ea
emarichard11-del <ton-email@gmail.com>
2026-05-24 08:16:42 -0400

    haute confiance minimum 3 matchs joués

--- server.py
+++ server.py
@@ -88,7 +88,7 @@ class Handler(BaseHTTPRequestHandler):
             over25 = min(78, max(25, round(avg_goals * 18 + 15)))
             win_market = max([('1', prob1), ('X', probX), ('2', prob2)], key=lambda x: x[1])
             best_market = win_market
-            high_confidence = best_market[1] >= 68
+            high_confidence = best_market[1] >= 68 and home_total >= 3 and away_total >= 3
             return {
                 'prob1': prob1, 'probX': probX, 'prob2': prob2,
                 'btts': btts, 'over25': over25,
